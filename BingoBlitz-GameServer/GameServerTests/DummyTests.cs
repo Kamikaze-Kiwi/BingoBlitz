afb9e02fcbfbbe0d6111a0c1b4c903aa2a344b1c
MaikelHendrikx1 <maikel.hendrikx@outlook.com>
2023-10-25 13:09:33 +0200

    Added CI pipeline for backend, added dummy testing project

---
+++ BingoBlitz-GameServer/GameServerTests/DummyTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer_Tests
+{
+    [TestClass]
+    public class DummyTests
+    {
+        [TestMethod]
+        public void DummyTest()
+        {
+            Assert.AreEqual(0, 0);
+        }
+    }
+}
